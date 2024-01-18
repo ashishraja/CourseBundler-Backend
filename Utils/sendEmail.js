1ff3e59337817a288101cc25288391d260efecaf
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-01-18 20:58:07 +0530

    mail-commit

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -5,6 +5,7 @@ const sendEmail = async (to,subject,text) => {
     const transport = createTransport({
         host:process.env.SMTP_HOST,
         port:process.env.SMTP_PORT,
+        secure:true,
         auth:{
                 user:process.env.SMTP_USER,
                 pass:process.env.SMTP_PASS,
