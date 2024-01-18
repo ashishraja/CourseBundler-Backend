84a7e295530c2dcba4022aafc9858629bcf96dd0
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-01-18 23:48:50 +0530

    commited

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -1,33 +1,30 @@
-import { createTransport } from "nodemailer";
+import nodemailer from 'nodemailer';
 
 const sendEmail = async (options) => {
-  const transport = createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure:true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    }
-  })
+  try {
+    const transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true, // Use true for SSL, false for other ports like 587
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
-  const emailOptions = {
-    from:"ashishsantani.it21@scet.ac.in",
-    to: options.email,
-    subject: options.subject,
-    text: options.message
+    const emailOptions = {
+      from: 'ashishsantani.it21@scet.ac.in',
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+
+    const info = await transport.sendMail(emailOptions);
+
+    console.log('Email sent:', info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
   }
-
-  await transport.sendMail(emailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
 };
 
-
-
 export default sendEmail;
-
